808307a1f5032a462beb28f1edf6f2c1b72b9e9a
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-30 13:54:12 -0300

    add auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet 
 import Link from 'next/link'
 import { ProfileRelationsBoxWrapper } from '../components/ProfileRelations'
 import { useEffect, useState } from 'react'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 
 function ProfileSidebar({ githubUser }) {
   return (
@@ -55,7 +57,7 @@ function ProfileRelationsBox({ title, items }) {
   )
 }
 
-export default function Home() {
+export default function Home({ githubUser }) {
   const [communities, setCommunities] = useState([])
   const [seguidores, setSeguidores] = useState([]);
 
@@ -93,7 +95,6 @@ export default function Home() {
     setCommunities(response.data.allCommunities)
   }, [])
 
-  const githubUser = 'eduardobravop'
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -215,4 +216,31 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+    .then((resposta) => resposta.json())
+
+  if (!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, // will be passed to the page component as props
+  }
 }
